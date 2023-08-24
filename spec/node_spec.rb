9bdcc77802518f0f9ad1a853ff499ab6f4111d87
Eric Belongea <eric.belongea@gmail.com>
2023-08-24 17:07:10 -0600

    Initial commit

---
+++ spec/node_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require "../lib/node"
+
+RSpec.describe Node do
+    it 'exists' do
+        node = Node.new("plop")
+
+        expect(node).to be_instance_of(Node)
+    end
+
+    it 'node can store data' do
+        node = Node.new("plop")
+
+        expect(node.data).to eq("plop")
+    end
+
+    it 'has an empty pointer' do 
+        node = Node.new("plop")
+
+        expect(node.next_node).to eq(nil)
+    end
+end
